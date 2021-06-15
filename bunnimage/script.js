dbef5bbbffb4dc24d8f57c76f71a58dc76954a6b
emsesc <emseschen@gmail.com>
2021-06-15 09:15:08 -0400

    w4s3

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,11 +1,26 @@
-function getImage(event) {
+async function getImage(event) {
     event.preventDefault()
     var myform = document.getElementById("myform")
     var payload = new FormData(myform);
     console.log(payload)
-    if (document.getElementById("username").value != '') {
+    var username = document.getElementById("username").value;
+    if (username != '') {
         $('#output').text("Thanks!")
+  
+        console.log("Posting your image...");
+        const resp = await fetch("https://priscool.azurewebsites.net/api/w3s2?code=5Rjd6l23aoqSnlYG6/9IIaRMVbvVdks7rXxglstZhl9aoi1CjO/ymQ==", {
+            method: 'POST',
+            headers: {
+                'codename' : username
+            },
+            body: payload
+        });
+  
+        var data = await resp.json();
+        console.log(data);
+        $('#output').text("Your image has been stored!")
     } else {
         alert("No name error.")
     }
   }
+  
